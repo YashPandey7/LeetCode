e655416ff5123f1fb7886fe3a050c2747d2d3570
Yash Pandey <87138730+YashPandey7@users.noreply.github.com>
2024-09-20 04:07:59 +0530

    Time: 3 ms (85.4%), Space: 16.6 MB (44.91%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -1,35 +1,22 @@
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
-        int val1 = 1, val2 = 1;
+        int val = 1;
         int n = nums.size();
         int ans = INT_MIN;
-        // for(int i=0; i<n; i++){
-        //     val = val*nums[i];
-        //     ans = max(ans, val);
-        //     if(nums[i] == 0){
-        //         val = 1;
-        //     }
-        // }
-        // val = 1;
-        // for(int i=n-1; i>=0; i--){
-        //     val *= nums[i];
-        //     ans = max(ans, val);
-        //     if(nums[i] == 0){
-        //         val = 1;
-        //     }
-        // }
-
         for(int i=0; i<n; i++){
-            val1 = val1*nums[i];
-            ans = max(ans, val1);
+            val = val*nums[i];
+            ans = max(ans, val);
             if(nums[i] == 0){
-                val1 = 1;
+                val = 1;
             }
-            val2 = val2*nums[n-1-i];
-            ans = max(ans, val2);
-            if(nums[n-1-i] == 0){
-                val2 = 1;
+        }
+        val = 1;
+        for(int i=n-1; i>=0; i--){
+            val *= nums[i];
+            ans = max(ans, val);
+            if(nums[i] == 0){
+                val = 1;
             }
         }
         return ans;
