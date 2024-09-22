c0bcee398404f365436ed44ae7d6c2db392801d4
Yash Pandey <87138730+YashPandey7@users.noreply.github.com>
2024-09-23 02:50:46 +0530

    Time: 55 ms (86.07%), Space: 29.4 MB (45.17%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -8,6 +8,9 @@ public:
         }
         sort(nums.begin(), nums.end());
         for(int i=0; i<n-2; i++){
+            if(nums[i] > 0){
+                break;
+            }
             if(i>0 && nums[i] == nums[i-1]){
                 continue;
             }
