60204e7136ac764073038b221734eb910f755468
Yash Pandey <87138730+YashPandey7@users.noreply.github.com>
2024-09-24 16:57:17 +0530

    Time: 7 ms (22.47%), Space: 12.3 MB (84.33%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -1,7 +1,24 @@
 class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
-        auto it = lower_bound(nums.begin(), nums.end(), target);
-        return (it- nums.begin());
+        // auto it = lower_bound(nums.begin(), nums.end(), target);
+        // return (it- nums.begin());
+
+        int l = 0, h = nums.size() - 1;
+        int ans = -1;
+        while(l <= h){
+            int mid = h - (h-l)/2;
+            if(nums[mid] > target){
+                h = mid - 1;
+            }
+            else if(nums[mid] == target){
+                return mid;
+            }
+            else{
+                l = mid +1;
+                ans = mid;
+            }
+        }
+        return ans+1;
     }
 };
