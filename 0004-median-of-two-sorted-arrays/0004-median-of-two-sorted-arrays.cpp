77412aed2f269675f2217a402bdbb3b19f5bff79
Yash Pandey <87138730+YashPandey7@users.noreply.github.com>
2024-10-03 01:21:23 +0530

    Time: 20 ms (78.92%), Space: 94.8 MB (52.68%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -2,34 +2,25 @@ class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
         int n = nums1.size(), m = nums2.size();
-        int nums[n+m];
+        vector<int> nums(n+m);
         int  i = 0, j = 0, k = 0;
         while(i<n && j<m){
             if(nums1[i] < nums2[j]){
-                nums[k] = nums1[i];
-                i++;
-                k++;
+                nums[k++] = nums1[i++];
             }
             else if(nums1[i] == nums2[j]){
-                nums[k] = nums1[i];
-                i++; k++;
-                nums[k] = nums2[j];
-                j++; k++;
+                nums[k++] = nums1[i++];
+                nums[k++] = nums2[j++];
             }
             else{
-                nums[k] = nums2[j];
-                j++; k++;
+                nums[k++] = nums2[j++];
             }
         }
         while(i < n){
-            nums[k] = nums1[i];
-            k++;
-            i++;
+            nums[k++] = nums1[i++];
         }
         while(j < m){
-            nums[k] = nums2[j];
-            j++;
-            k++;
+            nums[k++] = nums2[j++];
         }
 
         int size = n+m;
@@ -37,10 +28,6 @@ public:
             return nums[size/2];
         }
         else{
-            // double a = nums[size/2];
-            // double b = nums[(size/2) - 1];
-            // double res = (a+b)/2;
-            // return res;
             return (double)(nums[size/2] + nums[(size/2)-1])/2;
         }
 
